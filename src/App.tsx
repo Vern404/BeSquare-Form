7227377f796c9ac3bcb77fd1e1c7989976acf1ec
Vern <vern.yi@besquare.com.my>
2022-07-12 09:35:04 +0800

    added radio button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import * as React from "react";
-import "./style.css";
+import "./App.css";
 
 export default function App() {
   const [name, setName] = React.useState<string>("");
-  const [age, setAge] = React.useState<string | undefined>();
+  const [age, setAge] = React.useState<string | undefined>("");
   const [color, setColor] = React.useState<string | undefined>("");
+  const [gender, setGender] = React.useState<string | undefined>("male");
 
   const changeName = (event: React.ChangeEvent<HTMLInputElement>) => {
     setName(event.target.value);
@@ -14,8 +15,16 @@ export default function App() {
     setAge(event.target.value);
   };
 
-  const changeColor = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const changeColor = (event: React.ChangeEvent<HTMLSelectElement>) => {
     setColor(event.target.value);
+  };
+
+  const changeGender = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setGender(event.target.value);
+  };
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(name, age, gender, color);
   };
 
   return (
@@ -23,6 +32,24 @@ export default function App() {
       <form onSubmit={handleSubmit}>
         <Input name="Name" value={name} onChange={changeName} />
         <Input name="Age" value={age} onChange={changeAge} />
+        <div className="gender">
+          <input
+            name="gender"
+            type="radio"
+            value="male"
+            checked={gender === "male"}
+            onChange={changeGender}
+          />
+          <label className="gender"> Male</label>
+          <input
+            name="gender"
+            type="radio"
+            value="female"
+            checked={gender === "female"}
+            onChange={changeGender}
+          />
+          <label className="gender"> Female</label>
+        </div>
         <Select name="Favourite Color" value={color} onChange={changeColor} />
         <button type="submit">Submit</button>
         <button>Clear</button>
@@ -39,7 +66,7 @@ type InputType = {
 const Input = ({ value, onChange, name }: InputType) => {
   return (
     <div className="pa-16">
-      <label>(name)</label>
+      <label>{name}</label>
       <input value={value} onChange={onChange} />
     </div>
   );
@@ -48,14 +75,16 @@ const Input = ({ value, onChange, name }: InputType) => {
 type SelectType = {
   name: string;
   value: string | undefined;
-  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onChange: (event: React.ChangeEvent<HTMLSelectElement>) => void;
 };
-const Select = ({ value, onChange, name }: InputType) => {
+
+const Select = ({ value, onChange, name }: SelectType) => {
   return (
     <div className="pa-16">
-      <label>(name)</label>
-      <select value={value} onSelect={onChange}>
+      <label>{name}</label>
+      <select value={value} onChange={onChange}>
         <option value="red">Red</option>
+        <option value="blue">Blue</option>
       </select>
     </div>
   );
